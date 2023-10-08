50793e4e1d2abc758e4a3280e39dbcc523d40354
exisodd <ethantran004@yahoo.ca>
2023-10-07 21:31:48 -0400

    project setup

--- src/main/java/edu/vanier/controllers/MainAppController.java
+++ src/main/java/edu/vanier/controllers/MainAppController.java
@@ -1,31 +1,40 @@
-package edu.vanier.template.controllers;
+package edu.vanier.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 /**
  * Controller class of the MainApp's UI.
  *
- * @author frostybee
  */
 public class MainAppController {
 
     private final static Logger logger = LoggerFactory.getLogger(MainAppController.class);
+
     @FXML
-    Button btnClickMe;
+    private Button button1;
+
+    @FXML
+    private Button button2;
 
     @FXML
     public void initialize() {
         logger.info("Initializing MainAppController...");
-        btnClickMe.setOnAction((event) -> {
-            handleClickMe();
-        });
     }
 
-    private void handleClickMe() {
+    @FXML
+    void handleBtn1(ActionEvent event) {
         System.out.println("Click me called.");
-        logger.info("Click me button has been pressed...");        
+        logger.info("Click me button has been pressed...");
+    }
+
+    @FXML
+    void handleBtn2(ActionEvent event) {
+        System.out.println("Button 2 clicked.");
     }
 }
+
---
+++ src/main/java/edu/vanier/ui/MainApp.java
@@ -0,0 +1,42 @@
+package edu.vanier.ui;
+
+import edu.vanier.controllers.MainAppController;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Main entry point of the JavaFX application.
+ *
+ * @author Ethan Tran
+ * @author Mackenzie Rouchdy
+ * @author Zachary Tremblay
+ * @author Anton Lisunov
+ */
+public class MainApp extends Application {
+
+    private final static Logger logger = LoggerFactory.getLogger(MainApp.class);
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            logger.info("Bootstrapping the application...");
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainApp.fxml"));
+            Scene scene = new Scene(loader.load(), 1280, 720);
+            primaryStage.setScene(scene);
+            // primaryStage.sizeToScene();
+            primaryStage.show();
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
