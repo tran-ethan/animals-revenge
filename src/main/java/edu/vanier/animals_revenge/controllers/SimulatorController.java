70a32ec7c838eca122e87ac027f70bd0b3a84ea2
MackenzieRouchdy <mackenzierouchdy@gmail.com>
2023-12-03 19:20:10 -0500

    Revert "Revert "Linked sliders with text fields""
    This reverts commit ccece1febad556e7f506c92bff2c10cece8d92fd.

--- src/main/java/edu/vanier/animals_revenge/controllers/SimulatorController.java
+++ src/main/java/edu/vanier/animals_revenge/controllers/SimulatorController.java
@@ -27,12 +27,11 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.FileChooser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-
+import java.io.*;
 import static com.almasb.fxgl.dsl.FXGL.getGameWorld;
 import static com.almasb.fxgl.dsl.FXGL.spawn;
 
